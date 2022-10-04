985f3f9503eecd2920a7a47a03b8bf53cbf24e5b
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-10-04 14:16:59 -0300

    Testando requisito 7

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,15 +1,81 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
+import Loading from './Loading';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      album: '',
+      albumMusics: '',
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    console.log('Montado');
+    this.getMusic();
+  }
+
+  getMusic = async () => {
+    const { match } = this.props;
+    const { id } = match.params;
+    const album = await getMusics(id);
+    this.setState(
+      () => ({
+        album,
+      }),
+      this.initAlbum,
+    );
+  };
+
+  initAlbum = () => {
+    const { album } = this.state;
+    const albumMusics = album;
+    albumMusics.shift();
+    this.setState({ isLoading: false, albumMusics });
+  };
+
   render() {
+    const { album, isLoading, albumMusics } = this.state;
+    if (isLoading) return <Loading />;
+    const { collectionName, artistName, artworkUrl100 } = album[0];
     return (
       <div data-testid="page-album">
         <Header />
-        Album
+        <div>
+          <h4 data-testid="album-name">
+            Collection Name:
+            {' '}
+            {collectionName}
+          </h4>
+          <img src={ artworkUrl100 } alt={ collectionName } />
+          <h6 data-testid="artist-name">
+            Artist Name:
+            {artistName}
+          </h6>
+        </div>
+        {albumMusics.map((music, index) => (
+          <MusicCard
+            key={ index }
+            trackName={ music.trackName }
+            previewUrl={ music.previewUrl }
+          />
+        ))}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default Album;
