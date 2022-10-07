15f0b4d6090a406d82a1c2acb75143a256a3189e
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-10-06 21:17:51 -0300

    Alinhamento do loading

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Loading extends React.Component {
   render() {
-    return (<span>Carregando...</span>);
+    return (<span className="loading">Carregando...</span>);
   }
 }
 
